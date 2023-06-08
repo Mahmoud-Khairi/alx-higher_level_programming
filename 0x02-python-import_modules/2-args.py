47483ecb9bb4c901fde4dcdf462dc08b24d8f4ac
Mahmoud-khairi <khairimahmoud01@gmail.com>
2023-06-08 14:18:23 -0700

    fixing task 2 errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
         print('1 argument:')
         print('1: {:s}'.format(argv[1]))
     elif len(argv) > 2:
-        print('{:d} arguments:'.format(len(argv) - 2))
+        print('{:d} arguments:'.format(len(argv) - 3))
         for arg in range(1, len(argv)):
             print('{:d}: {:s}'.format(arg, argv[arg]))
