8c702d647504629b134d09803a1a265614dce681
Mahmoud-khairi <khairimahmoud01@gmail.com>
2023-06-14 17:11:05 -0700

    fixing errors

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number):
+def multiply_list_map(my_list=[], number=1):
     return list(map(lambda other_number: other_number * number, my_list))
